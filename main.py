a7184dc802ad3744009a99aab55e0ad6284187bf
Venca321 <login@vaclavparma.cz>
2023-05-01 08:40:40 +0200

    fix rotation and filters

--- main.py
+++ main.py
@@ -31,10 +31,18 @@ class Image():
         """
         Crops the image
         """
-        filtered_img = cv2.medianBlur(img, 81) #Filter showing approximate shape of the paper
+        ret, thresh_img = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY) #Convert to binary image
+        #cv2.imshow('Thresh', Image.resize(thresh_img, 0.15))
+        filtered_img = cv2.medianBlur(thresh_img, 101) #Filter showing approximate shape of the paper
+        #cv2.imshow('Filter', Image.resize(filtered_img, 0.15))
         edges_img = cv2.Canny(filtered_img, 100, 200) #Edge detection
+        #cv2.imshow('Edges', Image.resize(edges_img, 0.15))
         contours, hierarchy = cv2.findContours(edges_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         x, y, w, h = cv2.boundingRect(contours[0])
+
+        if w < 500 or h < 500: #If too small, probably poorly defined edges
+            return img
+
         return img[y:y+h, x:x+w] #Crop
 
 class Engine():
@@ -47,7 +55,7 @@ class Engine():
         Image processing for the required data
         """
         preprocessed_img = Engine.image_preprocessing(file)
-        cv2.imshow('Cropped, filtered Image', Image.resize(preprocessed_img, 0.3))
+        cv2.imshow('Cropped, filtered Image', Image.resize(preprocessed_img, 0.2))
         cv2.waitKey(0) #Q for closing the window
         cv2.destroyAllWindows()
 
@@ -56,14 +64,22 @@ class Engine():
         Basic image processing
         """
         input_img = cv2.imread(file)
-        if input_img.shape[0] > input_img.shape[1]: 
-            input_img = Image.rotate(input_img) #Turn if needed
-
+        cv2.imshow('Input', Image.resize(input_img, 0.2))
         gray_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY) #Converts to shades of gray
-        ret, thresh_img = cv2.threshold(gray_img, 127, 255, cv2.THRESH_BINARY) #Convert to binary image
+        processed_img = cv2.medianBlur(Image.crop(gray_img), 3)
+        ret, processed_img = cv2.threshold(processed_img, 110, 235, cv2.THRESH_BINARY) #Convert to binary image
 
-        working_img = cv2.medianBlur(Image.crop(thresh_img), 3)
-        return working_img
+        if processed_img.shape[0] > processed_img.shape[1]: 
+            processed_img = Image.rotate(processed_img) #Turn if needed
+
+        #TODO: Image.flip() if needed
+
+        return processed_img
 
 if "__main__" == __name__:
-    Engine.process("testImg.jpg")
+    Engine.process(f"TestImg/img0.jpg")
+    Engine.process(f"TestImg/img1.jpg")
+    Engine.process(f"TestImg/img2.jpg")
+    Engine.process(f"TestImg/img3.jpg")
+    Engine.process(f"TestImg/img4.jpg")
+    Engine.process(f"TestImg/img5.jpg")
