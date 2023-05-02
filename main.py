f10d3915a5a362552442b4cd8aa87b4eda659a61
venca321 <login@vaclavparma.cz>
2023-05-02 08:38:05 +0200

    Fix noqr bug, improve filters

--- main.py
+++ main.py
@@ -34,15 +34,14 @@ class Image():
         """
         Crops the image
         """
-        ret, thresh_img = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY) #Convert to binary image
+        _, thresh_img = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY) #Convert to binary image
         filtered_img = cv2.medianBlur(thresh_img, 81) #Filter showing approximate shape of the paper
         edges_img = cv2.Canny(filtered_img, 100, 200) #Edge detection
-        contours, hierarchy = cv2.findContours(edges_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours, _ = cv2.findContours(edges_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         x, y, w, h = cv2.boundingRect(contours[0])
         
         if debug_mode: 
-            cv2.imshow('Thresh', Image.resize(thresh_img, image_scale))
-            cv2.imshow('Filter', Image.resize(filtered_img, image_scale))
+            #cv2.imshow('Thresh', Image.resize(thresh_img, image_scale))
             cv2.imshow('Edges', Image.resize(edges_img, image_scale))
 
         if h < (img.shape[0]/3) or w < (img.shape[1]/3): #If too small, probably poorly defined edges
@@ -66,25 +65,28 @@ class Qr():
         If needed rotates img and get qr data, returns img, data
         """
         qr_data, x, y = None, None, None
+
+        _, binary_img = cv2.threshold(img, 120, 220, cv2.THRESH_BINARY) #Convert to binary image
+
         qr_decoder = cv2.QRCodeDetector()
-        data, bbox, _ = qr_decoder.detectAndDecode(img)
+        data, bbox, _ = qr_decoder.detectAndDecode(binary_img)
 
         if bbox is None: #If qr not decoded try flip
-            rotated_img = Image.flip(img)
-            data, bbox, _ = qr_decoder.detectAndDecode(img)
+            rotated_img = Image.flip(binary_img)
+            data, bbox, _ = qr_decoder.detectAndDecode(rotated_img)
             if bbox is not None:
                 qr_data = data
-                img = rotated_img
+                img = Image.flip(img)
                 x, y = bbox[0][0] #qrcode cords
 
         else: 
             x, y = bbox[0][0] #qrcode cords
             qr_data = data
 
-        if x > img.shape[1]/2 or y > img.shape[0]/2: #if not in top right corner, flip it
-            img = Image.flip(img)
-
         if qr_data is not None:
+            if x > img.shape[1]/2 or y > img.shape[0]/2: #if not in top right corner, flip it
+                img = Image.flip(img)
+
             return img, qr_data
         
         raise NotImplementedError #No readable qrcode on img
@@ -99,11 +101,15 @@ class Engine():
         Image processing for the required data
         """
         preprocessed_img = Engine.image_preprocessing(file)
-        img, qr_data = Qr.process(preprocessed_img)
-
+        img, qr_data = Qr.process(preprocessed_img) #Rotate img if needed
+        
         print(qr_data)
+        
+        _, img = cv2.threshold(img, 125, 210, cv2.THRESH_BINARY) #binary img (shades bug)
+        # Shades bug --> binary is readable, depending on light
+        # I can try change values if OCR cannot read correctly
 
-        cv2.imshow('Processed', Image.resize(img, image_scale))
+        cv2.imshow('Processed', Image.resize(img, 0.3)) #image_scale
         cv2.waitKey(0) #Q for closing the window
         cv2.destroyAllWindows()
 
@@ -114,7 +120,6 @@ class Engine():
         input_img = cv2.imread(file)
         gray_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY) #Converts to shades of gray
         processed_img = cv2.medianBlur(Image.crop(gray_img), 3)
-        ret, processed_img = cv2.threshold(processed_img, 110, 235, cv2.THRESH_BINARY) #Convert to binary image
 
         if processed_img.shape[0] > processed_img.shape[1]: 
             processed_img = Image.rotate(processed_img) #Turn if needed
